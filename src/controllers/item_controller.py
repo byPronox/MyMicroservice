ed12fa79404e1b3301fce4d7109850ef091e9b9c
byPronox <bypronoxyt@gmail.com>
2025-06-07 13:25:46 -0500

    UPDATE: Item controler

--- src/controllers/item_controller.py
+++ src/controllers/item_controller.py
@@ -1,13 +1,18 @@
-from abc import ABC, abstractmethod
-from typing import List
+from fastapi import APIRouter, Depends
+from src.services.item_service import ItemService
+from src.repositories.item_repository import ItemRepository
 from src.models.item_model import Item
 
-class IItemRepository(ABC):
+router = APIRouter()
 
-    @abstractmethod
-    def fetch_all(self) -> List[Item]:
-        pass
+def get_service():
+    repository = ItemRepository()
+    return ItemService(repository)
 
-    @abstractmethod
-    def save(self, item: Item) -> Item:
-        pass
+@router.get("/items")
+def get_items(service: ItemService = Depends(get_service)):
+    return service.get_all_items()
+
+@router.post("/items")
+def create_item(item: Item, service: ItemService = Depends(get_service)):
+    return service.add_item(item)
